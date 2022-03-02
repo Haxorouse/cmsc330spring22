1555b1771d31d1cb19fe26797b7485a16a900e51
Haxorouse <45926268+Haxorouse@users.noreply.github.com>
2022-03-02 02:23:29 -0500

    Update basics.ml
    p2b progress

--- project2a/src/basics.ml
+++ project2a/src/basics.ml
@@ -2,27 +2,59 @@
 (* Part 1: Non-Recursive Functions *)
 (***********************************)
 
-let rev_tup tup = failwith "unimplemented"
+let rev_tup tup = 
+    match tup with 
+    (a, b, c) -> (c, b, a);;
 
-let is_odd x = failwith "unimplemented"
+let is_odd x =
+    if (x mod 2) = 0 then false else true;;
 
-let area x y = failwith "unimplemented"
+let area x y = 
+match x with (a, b) ->
+match y with (c, d) ->
+    abs((a-(c)) * (b-(d)));;
 
-let volume x y = failwith "unimplemented"
+let volume x y = 
+match x with (a, b, c) ->
+match y with (d, e, f) ->
+    abs((a-(d)) * (b-(e)) * (c-(f)));;
 
 (*******************************)
 (* Part 2: Recursive Functions *)
 (*******************************)
 
-let rec fibonacci n = failwith "unimplemented"
+let rec fibonacci n =
+    match n with
+    0 -> 0
+    | 1 -> 1
+    | _ ->
+    (fibonacci (n-1)) + (fibonacci (n-2));;
 
-let rec pow x y = failwith "unimplemented"
+let rec pow x y =
+    match y with
+    0 -> 1
+    | _ ->
+    x * (pow x (y-1));;
 
-let rec log x y = failwith "unimplemented"
+let rec log x y = 
+    if(y/x) < 1 then 0
+    else 1 + (log x (y/x));;
 
-let rec gcf x y = failwith "unimplemented"
+let rec gcf x y = 
+    if x = 0 then y
+    else gcf (y mod x) x;;
 
-let rec is_prime x = failwith "unimplemented"
+let rec is_prime_recurse x d =
+    if (x/d) < d then true
+    else if (x mod d) = 0 then false
+    else is_prime_recurse x (d+1);;
+
+let rec is_prime x =
+    match x with
+    0 -> false
+    | 1 -> false
+    | _ -> if x < 0 then false
+    else is_prime_recurse x 2;;
 
 (*****************)
 (* Part 3: Lists *)
