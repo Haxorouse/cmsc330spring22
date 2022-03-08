34895f06f9f42d2aa178535ba789148c554ef407
Haxorouse <45926268+Haxorouse@users.noreply.github.com>
2022-03-08 04:04:35 -0500

    p2b WIP 1

--- project2b/src/data.ml
+++ project2b/src/data.ml
@@ -11,16 +11,41 @@ type int_tree =
 let empty_int_tree = IntLeaf
 
 let rec int_insert x t =
-  failwith "unimplemented"
+  match t with
+    | IntLeaf -> IntNode(x, IntLeaf, IntLeaf, IntLeaf)
+    | IntNode (low, IntLeaf, _, _) -> 
+      if x < low then IntNode (x, low, IntLeaf, IntLeaf, IntLeaf)
+      else if x > low then IntNode (low, x, IntLeaf, IntLeaf, IntLeaf)
+      else t
+    | IntNode (low, high, fst, scd, thd) ->
+      if x < low then int_insert x fst
+      else if x > high then int_insert x thd
+      else if x > low && x < high then int_insert x scd
+      else t;;
 
 let rec int_mem x t =
-  failwith "unimplemented"
+  match t with
+    | IntLeaf -> false
+    | IntNode (low, IntLeaf, _, _) -> if x = low then true else false
+    | IntNode (low, high, fst, scd, thd) -> 
+      if x = low || x = high then true
+      else if x < low then int_mem x fst
+      else if x > high then int_mem x thd
+      else int_mem x scd;;
 
 let rec int_size t =
-  failwith "unimplemented"
+   match t with
+    | IntLeaf -> 0
+    | IntNode (low, IntLeaf, _, _) -> 1
+    | IntNode (low, high, fst, scd, thd) ->
+      2 + int_size fst + int_size scd + int_size thd;;
 
 let rec int_max t =
-  failwith "unimplemented"
+  match t with
+    | IntLeaf -> raise (Invalid_argument("int_max"))
+    | IntNode (low, IntLeaf, _, _) -> low
+    | IntNode (low, high, _, _, IntLeaf) -> high
+    | IntNode (low, high, _, _, thd) -> int_max thd;;
 
 (*******************************)
 (* Part 3: Three-Way Search Tree-Based Map *)
