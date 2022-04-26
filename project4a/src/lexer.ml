409215a21b54515c3dc3b914bb345bdc9cf6be5d
Haxorouse <Haxorouse@gmail.com>
2022-04-26 01:26:17 -0400

    def regexs

--- project4a/src/lexer.ml
+++ project4a/src/lexer.ml
@@ -2,4 +2,39 @@ open TokenTypes
 
 (* Part 1: Lexer - IMPLEMENT YOUR CODE BELOW *)
 
-let tokenize input = failwith "unimplemented"
+let ReRParen = Str.regexp "("
+let ReLParen = Str.regexp ")"
+let ReEqu = Str.regexp "="
+let ReNotEqu = Str.regexp "<>"
+let ReGrtr = Str.regexp ">"
+let ReLss = Str.regexp "<"
+let ReGrEq = Str.regexp ">="
+let ReGrLs = Str.regexp "<="
+let ReOr = Str.regexp "||"
+let ReAnd = Str.regexp "&&"
+let ReNot = Str.regexp "not"
+let ReIf = Str.regexp "if"
+let ReThn = Str.regexp "then"
+let ReEls = Str.regexp "else"
+let ReAdd = Str.regexp "\\+"
+let ReSub = Str.regexp "-"
+let ReMult = Str.regexp "\*"
+let ReDiv = Str.regexp "/"
+let ReCnct = Str.regexp "\\^"
+let ReLet = Str.regexp "let"
+let ReDef = Str.regexp "def"
+let ReIn = Str.regexp "in"
+let ReFun = Str.regexp "fun"
+let ReArw = Str.regexp "->"
+let ReDubSmi = Str.regexp ";;"
+let ReBool = Str.regexp "true|false"
+let ReInt = Str.regexp "[0-9]+"
+let ReStr = Str.regexp "\"[^\"]*\""
+let ReID = Str.regexp "[a-zA-Z][a-zA-Z0-9]*"
+
+let tokenize input = 
+    let rec scan pos s =
+    if pos >= String.length s then
+        []
+    else
+        if
