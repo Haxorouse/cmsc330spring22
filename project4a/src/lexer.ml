8c5e962c16c281ba33d97f313b32043ff139c66c
Haxorouse <Haxorouse@gmail.com>
2022-04-26 01:53:51 -0400

    Yandere dev level

--- project4a/src/lexer.ml
+++ project4a/src/lexer.ml
@@ -9,7 +9,7 @@ let ReNotEqu = Str.regexp "<>"
 let ReGrtr = Str.regexp ">"
 let ReLss = Str.regexp "<"
 let ReGrEq = Str.regexp ">="
-let ReGrLs = Str.regexp "<="
+let ReLsEq = Str.regexp "<="
 let ReOr = Str.regexp "||"
 let ReAnd = Str.regexp "&&"
 let ReNot = Str.regexp "not"
@@ -18,7 +18,7 @@ let ReThn = Str.regexp "then"
 let ReEls = Str.regexp "else"
 let ReAdd = Str.regexp "\\+"
 let ReSub = Str.regexp "-"
-let ReMult = Str.regexp "\*"
+let ReMult = Str.regexp "\\*"
 let ReDiv = Str.regexp "/"
 let ReCnct = Str.regexp "\\^"
 let ReLet = Str.regexp "let"
@@ -31,10 +31,70 @@ let ReBool = Str.regexp "true|false"
 let ReInt = Str.regexp "[0-9]+"
 let ReStr = Str.regexp "\"[^\"]*\""
 let ReID = Str.regexp "[a-zA-Z][a-zA-Z0-9]*"
-
+let ReWht = Str.regexp "\s" (*should match all white space characters*)
 let tokenize input = 
     let rec scan pos s =
     if pos >= String.length s then
         []
     else
-        if
+        if Str.string_match ReWht s pos then
+            scan (pos + 1) s
+        else if Str.string_match ReRParen s pos then
+
+        else if Str.string_match ReLParen s pos then
+
+        else if Str.string_match ReEqu s pos then
+
+        else if Str.string_match ReNotEqu s pos then
+
+        else if Str.string_match ReGrtr s pos then 
+
+        else if Str.string_match ReLss s pos then
+
+        else if Str.string_match ReGrEq s pos then
+
+        else if Str.string_match ReLsEq s pos then
+
+        else if Str.string_match ReOr s pos then
+
+        else if Str.string_match ReAnd s pos then
+
+        else if Str.string_match ReNot s pos then
+
+        else if Str.string_match ReIf s pos then
+
+        else if Str.string_match ReThn s pos then
+
+        else if Str.string_match ReEls s pos then
+
+        else if Str.string_match ReAdd s pos then
+
+        else if Str.string_match ReSub s pos then
+
+        else if Str.string_match ReMult s pos then
+
+        else if Str.string_match ReDiv s pos then
+
+        else if Str.string_match ReCnct s pos then
+
+        else if Str.string_match ReLet s pos then
+
+        else if Str.string_match ReDef s pos then
+
+        else if Str.string_match ReIn s pos then
+
+        else if Str.string_match ReFun s pos then
+
+        else if Str.string_match ReArw s pos then
+
+        else if Str.string_match ReDubSmi s pos then
+
+        else if Str.string_match ReBool s pos then
+
+        else if Str.string_match ReInt s pos then
+
+        else if Str.string_match ReStr s pos then
+
+        else if Str.string_match ReID s pos then 
+
+        else
